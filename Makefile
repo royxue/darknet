10c9d4db519316d98da706a40d65c8bb780c8bcf
Lijun Xue <lxue@tesla.com>
2017-04-03 09:50:58 -0700

    Upload personal settings

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-GPU=0
-CUDNN=0
+GPU=1
+CUDNN=1
 OPENCV=0
 DEBUG=0
 
@@ -10,7 +10,7 @@ ARCH= -gencode arch=compute_20,code=[sm_20,sm_21] \
       -gencode arch=compute_52,code=[sm_52,compute_52]
 
 # This is what I use, uncomment if you know your arch and want to specify
-ARCH=  -gencode arch=compute_52,code=compute_52
+# ARCH=  -gencode arch=compute_52,code=compute_52
 
 VPATH=./src/
 EXEC=darknet
--- src/darknet.c
+++ src/darknet.c
@@ -401,7 +401,7 @@ int main(int argc, char **argv)
     } else if (0 == strcmp(argv[1], "detect")){
         float thresh = find_float_arg(argc, argv, "-thresh", .24);
         char *filename = (argc > 4) ? argv[4]: 0;
-        test_detector("cfg/coco.data", argv[2], argv[3], filename, thresh, .5);
+        test_detector("cfg/qr.data", argv[2], argv[3], filename, thresh, .5);
     } else if (0 == strcmp(argv[1], "cifar")){
         run_cifar(argc, argv);
     } else if (0 == strcmp(argv[1], "go")){
